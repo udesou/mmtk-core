10605faf6b3d628696072892337c357e567d853e
Zixian Cai <u5937495@anu.edu.au>
2023-11-03 06:08:21 +0000

    Finish implementing TIB

--- tools/heapdump/src/main.rs
+++ tools/heapdump/src/main.rs
@@ -3,14 +3,13 @@ extern crate lazy_static;
 
 use anyhow::Result;
 use prost::Message;
-use std::ptr;
+use std::sync::{Arc, Mutex};
 use std::{collections::HashMap, fs::File, io::Read};
-use std::sync::{Mutex, Arc};
 
 include!(concat!(env!("OUT_DIR"), "/mmtk.util.sanity.rs"));
 
 lazy_static! {
-    static ref TIBS: Mutex<HashMap<u64, Arc<TIB>>> = Mutex::new(HashMap::new());
+    static ref TIBS: Mutex<HashMap<u64, Arc<Tib>>> = Mutex::new(HashMap::new());
 }
 
 fn wrap_libc_call<T: PartialEq>(f: &dyn Fn() -> T, expect: T) -> Result<()> {
@@ -40,20 +39,48 @@ fn mmap_fixed(start: u64, size: usize, prot: libc::c_int, flags: libc::c_int) ->
 }
 
 #[repr(C)]
-struct TIB {
+struct Tib {
     is_objarray: bool,
-    size: u64,
     oop_map_blocks: Vec<OopMapBlock>,
 }
 
-impl TIB {
-    fn no_ref(klass: u64, size: u64) -> Arc<TIB> {
-        // if TIBS
+impl Tib {
+    fn insert_with_cache(klass: u64, tib: impl FnOnce() -> Tib) -> Arc<Tib> {
         let mut tibs = TIBS.lock().unwrap();
-        if !tibs.contains_key(&klass) {
-            tibs.insert(klass, Arc::new(TIB { is_objarray: false, size, oop_map_blocks: vec![]}));
-        }
+        tibs.entry(klass).or_insert_with(|| Arc::new(tib()));
         tibs.get(&klass).unwrap().clone()
+    }
+
+    fn objarray(klass: u64) -> Arc<Tib> {
+        Self::insert_with_cache(klass, || {
+            Tib {
+                is_objarray: true,
+                oop_map_blocks: vec![],
+            }
+        })
+    }
+
+    fn non_objarray(klass: u64, obj: &HeapObject) -> Arc<Tib> {
+        Self::insert_with_cache(klass, || {
+            let mut oop_map_blocks: Vec<OopMapBlock> = vec![];
+            for e in &obj.edges {
+                if let Some(o) = oop_map_blocks.last_mut() {
+                    if e.slot == o.offset + o.count * 8 {
+                        o.count += 1;
+                        continue;
+                    }
+                }
+                oop_map_blocks.push(OopMapBlock {
+                    offset: e.slot - obj.start,
+                    count: 1,
+                });
+            }
+
+            Tib {
+                is_objarray: true,
+                oop_map_blocks,
+            }
+        })
     }
 }
 
@@ -73,30 +100,31 @@ fn main() -> Result<()> {
         println!("Mapping {} at 0x{:x}", s.name, s.start);
         dzmmap_noreplace(s.start, (s.end - s.start) as usize)?;
     }
-    for o in heapdump.objects {
+    for o in &heapdump.objects {
         unsafe {
             std::ptr::write::<u64>((o.start + 8) as *mut u64, o.start);
         }
-        let mut tib = ptr::null();
-        if !o.is_objarray {
-            if o.edges.len() == 0 {
-                tib = Arc::as_ptr(&TIB::no_ref(o.klass, o.size));
-            }
-        }
+        let tib = if o.is_objarray {
+            Arc::as_ptr(&Tib::objarray(o.klass))
+        } else {
+            Arc::as_ptr(&Tib::non_objarray(o.klass, o))
+        };
         // println!("Klass: 0x{:x}, TIB: 0x{:x}", o.klass, tib as u64);
-        unsafe { std::ptr::write::<u64>((o.start + 8) as *mut u64, tib as u64); }
+        unsafe {
+            std::ptr::write::<u64>((o.start + 8) as *mut u64, tib as u64);
+        }
         if o.is_objarray {
             unsafe {
                 std::ptr::write::<u64>((o.start + 16) as *mut u64, o.objarray_length);
             }
         }
-        for e in o.edges {
+        for e in &o.edges {
             unsafe {
                 std::ptr::write::<u64>(e.slot as *mut u64, e.objref);
             }
         }
     }
-    println!("Finish deserializing the heapdump");
+    println!("Finish deserializing the heapdump, {} objects", heapdump.objects.len());
 
     Ok(())
 }
