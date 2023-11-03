174c4f6fece6f7948e42ccd69ea15f04ba54ca36
Zixian Cai <u5937495@anu.edu.au>
2023-11-03 06:59:36 +0000

    Finish array scanning and transitive closure

--- tools/heapdump/src/main.rs
+++ tools/heapdump/src/main.rs
@@ -3,7 +3,9 @@ extern crate lazy_static;
 
 use anyhow::Result;
 use prost::Message;
+use std::collections::VecDeque;
 use std::sync::{Arc, Mutex};
+use std::time::Instant;
 use std::{collections::HashMap, fs::File, io::Read};
 
 include!(concat!(env!("OUT_DIR"), "/mmtk.util.sanity.rs"));
@@ -39,6 +41,7 @@ fn mmap_fixed(start: u64, size: usize, prot: libc::c_int, flags: libc::c_int) ->
 }
 
 #[repr(C)]
+#[derive(Debug)]
 struct Tib {
     is_objarray: bool,
     oop_map_blocks: Vec<OopMapBlock>,
@@ -52,11 +55,9 @@ impl Tib {
     }
 
     fn objarray(klass: u64) -> Arc<Tib> {
-        Self::insert_with_cache(klass, || {
-            Tib {
-                is_objarray: true,
-                oop_map_blocks: vec![],
-            }
+        Self::insert_with_cache(klass, || Tib {
+            is_objarray: true,
+            oop_map_blocks: vec![],
         })
     }
 
@@ -65,7 +66,7 @@ impl Tib {
             let mut oop_map_blocks: Vec<OopMapBlock> = vec![];
             for e in &obj.edges {
                 if let Some(o) = oop_map_blocks.last_mut() {
-                    if e.slot == o.offset + o.count * 8 {
+                    if e.slot == obj.start + o.offset + o.count * 8 {
                         o.count += 1;
                         continue;
                     }
@@ -77,7 +78,7 @@ impl Tib {
             }
 
             Tib {
-                is_objarray: true,
+                is_objarray: false,
                 oop_map_blocks,
             }
         })
@@ -85,9 +86,68 @@ impl Tib {
 }
 
 #[repr(C)]
+#[derive(Debug)]
 struct OopMapBlock {
     offset: u64,
     count: u64,
+}
+
+unsafe fn trace_object(o: u64) -> bool {
+    // println!("Trace object: 0x{:x}", o as u64);
+    if o == 0 {
+        // skip null
+        return false;
+    }
+    // Return false if already marked
+    let mark_word = o as *mut u64;
+    if *mark_word != 0 {
+        false
+    } else {
+        *mark_word = 1;
+        true
+    }
+}
+
+unsafe fn scan_object(o: u64, mark_queue: &mut VecDeque<u64>) {
+    let tib_ptr = *((o as *mut u64).wrapping_add(1) as *const *const Tib);
+    if tib_ptr.is_null() {
+        panic!("Object 0x{:x} has a null tib pointer", o as u64);
+    }
+    let tib: &Tib = &*tib_ptr;
+    // println!("Tib: {:?}", tib);
+    if tib.is_objarray {
+        let objarray_length = *((o as *mut u64).wrapping_add(2) as *const u64);
+        // println!("Objarray length: {}", objarray_length);
+        for i in 0..objarray_length {
+            let slot = (o as *mut u64).wrapping_add(3 + i as usize);
+            mark_queue.push_back(*slot);
+        }
+    }
+}
+
+unsafe fn transitive_closure(roots: &[RootEdge]) {
+    let start = Instant::now();
+    // A queue of objref (possibly null)
+    // aka node enqueuing
+    let mut mark_queue: VecDeque<u64> = VecDeque::new();
+    for root in roots {
+        mark_queue.push_back(root.objref);
+    }
+    let mut marked_object: u64 = 0;
+    while let Some(o) = mark_queue.pop_front() {
+        if trace_object(o) {
+            // not previously marked, now marked
+            // now scan
+            marked_object += 1;
+            scan_object(o, &mut mark_queue);
+        }
+    }
+    let elapsed = start.elapsed();
+    println!(
+        "Finished marking {} objects in {} ms",
+        marked_object,
+        elapsed.as_micros() as f64 / 1000f64
+    );
 }
 
 fn main() -> Result<()> {
@@ -100,6 +160,7 @@ fn main() -> Result<()> {
         println!("Mapping {} at 0x{:x}", s.name, s.start);
         dzmmap_noreplace(s.start, (s.end - s.start) as usize)?;
     }
+    let start = Instant::now();
     for o in &heapdump.objects {
         unsafe {
             std::ptr::write::<u64>((o.start + 8) as *mut u64, o.start);
@@ -109,7 +170,10 @@ fn main() -> Result<()> {
         } else {
             Arc::as_ptr(&Tib::non_objarray(o.klass, o))
         };
-        // println!("Klass: 0x{:x}, TIB: 0x{:x}", o.klass, tib as u64);
+        // println!(
+        //     "Object: 0x{:x}, Klass: 0x{:x}, TIB: 0x{:x}",
+        //     o.start, o.klass, tib as u64
+        // );
         unsafe {
             std::ptr::write::<u64>((o.start + 8) as *mut u64, tib as u64);
         }
@@ -124,7 +188,14 @@ fn main() -> Result<()> {
             }
         }
     }
-    println!("Finish deserializing the heapdump, {} objects", heapdump.objects.len());
-
+    let elapsed = start.elapsed();
+    println!(
+        "Finish deserializing the heapdump, {} objects in {} ms",
+        heapdump.objects.len(),
+        elapsed.as_micros() as f64 / 1000f64
+    );
+    unsafe {
+        transitive_closure(&heapdump.roots);
+    }
     Ok(())
 }
